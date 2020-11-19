da1da1f92bc1d28d1aa6b84ed501b5ffde16bf1f
diegosl <dsl_94@hotmail.com>
2020-11-19 10:37:59 -0300

    Se agrego variable e

--- app/src/main/java/com/example/bottomsheetejemplo/MainActivity.kt
+++ app/src/main/java/com/example/bottomsheetejemplo/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         val b = 2
         val c = 3
         val d = 4
+        val e = 5
 
         btnBottomSheet.setOnClickListener {
             val bottomSheetFragment = MiBottomSheet()
